39576efd08e27d5e4974523ce6ff0d11b691c678
Igor Sarwiński <igor.s@onet.pl>
2023-10-16 11:06:13 +0200

    Initial commit

---
+++ node_modules/@gluestack-ui/pressable/lib/typescript/Pressable.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { PressableProps } from 'react-native';
+declare function Pressable<T>(StyledPressable: React.ComponentType<T>): React.ForwardRefExoticComponent<React.PropsWithoutRef<T & Omit<PressableProps, "children"> & {
+    children?: React.ReactNode | (({ hovered, pressed, focused, focusVisible, disabled, }: {
+        hovered?: boolean | undefined;
+        pressed?: boolean | undefined;
+        focused?: boolean | undefined;
+        focusVisible?: boolean | undefined;
+        disabled?: boolean | undefined;
+    }) => React.ReactNode);
+}> & React.RefAttributes<unknown>>;
+export default Pressable;
+//# sourceMappingURL=Pressable.d.ts.map
---
+++ node_modules/@gluestack-ui/select/lib/module/SelectInput.js
@@ -0,0 +1,36 @@
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+import React, { forwardRef, useEffect } from 'react';
+import { SelectContext } from './SelectContext';
+export const SelectInput = StyledSelectInput => /*#__PURE__*/forwardRef((_ref, ref) => {
+  let {
+    placeholder: placeholderProp,
+    ...props
+  } = _ref;
+  const {
+    setValue,
+    value,
+    label,
+    isDisabled,
+    placeholder,
+    setPlaceholder
+  } = React.useContext(SelectContext);
+  useEffect(() => {
+    setPlaceholder && setPlaceholder(placeholderProp);
+  }, [placeholderProp, setPlaceholder]);
+  return /*#__PURE__*/React.createElement(StyledSelectInput, _extends({
+    ref: ref,
+    states: {
+      disabled: isDisabled
+    },
+    disabled: isDisabled,
+    "aria-hidden": true,
+    editable: false,
+    focusable: false,
+    importantForAccessibility: "no",
+    placeholder: placeholder,
+    value: label ? label : value ? value : '',
+    pointerEvents: "none",
+    onChangeText: text => setValue(text)
+  }, props));
+});
+//# sourceMappingURL=SelectInput.js.map
