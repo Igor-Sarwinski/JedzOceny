f3753714382171077d8913415480e3dd204e7de2
Igor Sarwiński <igor.s@onet.pl>
2023-10-25 13:11:43 +0200

    dodanie nawigacji dolnej

--- App.tsx
+++ App.tsx
@@ -1,148 +1,116 @@
-import { config } from '@gluestack-ui/config';
-import { Box, GluestackUIProvider, Text } from '@gluestack-ui/themed';
-import { ScrollView } from 'react-native';
-import Gradient from './assets/Icons/Gradient';
-import DocumentData from './assets/Icons/DocumentData';
-import LightBulbPerson from './assets/Icons/LightbulbPerson';
-import Rocket from './assets/Icons/Rocket';
-import Logo from './assets/Icons/Logo';
+import {config} from '@gluestack-ui/config';
+import {Box, Button, ButtonText, GluestackUIProvider, Text} from '@gluestack-ui/themed';
+import {DefaultTheme, NavigationContainer,DarkTheme} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// @ts-ignore
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {useColorScheme} from "react-native";
 
-export default function App() {
-  return (
-    <GluestackUIProvider config={config}>
-      <Home />
-    </GluestackUIProvider>
-  );
-}
 
-const Home = () => {
-  return <Container />;
-};
-
-const FeatureCard = ({ iconSvg: IconSvg, name, desc }: any) => {
-  return (
-    <Box
-      flexDirection="column"
-      borderWidth={1}
-      borderColor="$borderDark700"
-      sx={{
-        _web: {
-          flex: 1,
-        },
-      }}
-      m="$2"
-      p="$4"
-      rounded="$md"
-    >
-      <Box alignItems="center" display="flex" flexDirection="row">
-        {/* <Image source={iconSvg} alt="document" width={22} height={22} /> */}
-        <Text>
-          <IconSvg />
-        </Text>
-        <Text fontSize={22} color="$white" fontWeight="500" ml="$2">
-          {name}
-        </Text>
-      </Box>
-      <Text color="$textDark400" mt="$2">
-        {desc}
-      </Text>
+const HomeScreen = () => (
+    <Box flex={1} justifyContent="center" alignItems="center">
+        <Text>Strona główna</Text>
+            <Button
+                sx={{
+                    bg: '$red400',
+                }}>
+                <ButtonText>Hello world</ButtonText>
+            </Button>
     </Box>
-  );
+);
+
+const DocsScreen = () => (
+    <Box flex={1} justifyContent="center" alignItems="center">
+        <Text>Dokumentacja</Text>
+    </Box>
+);
+
+const LearnScreen = () => (
+    <Box flex={1} justifyContent="center" alignItems="center">
+        <Text>Nauka</Text>
+    </Box>
+);
+
+const DeployScreen = () => (
+    <Box flex={1} justifyContent="center" alignItems="center">
+        <Text>Wdrożenie</Text>
+    </Box>
+
+);
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+    const scheme = useColorScheme();
+    return (
+        <GluestackUIProvider config={config}>
+            <NavigationContainer theme={DarkTheme}>
+                <Tab.Navigator>
+                    <Tab.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{
+                            title: 'Strona główna',
+                            tabBarIcon: ({color, size}) => (
+                                <Icon name="home" color={color} size={size}/>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Docs"
+                        component={DocsScreen}
+                        options={{
+                            title: 'Dokumentacja',
+                            tabBarIcon: ({color, size}) => (
+                                <Icon name="book" color={color} size={size}/>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Learn"
+                        component={LearnScreen}
+                        options={{
+                            title: 'Nauka',
+                            tabBarIcon: ({color, size}) => (
+                                <Icon name="graduation-cap" color={color} size={size}/>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Deploy"
+                        component={DeployScreen}
+                        options={{
+                            title: 'Wdrożenie',
+                            tabBarIcon: ({color, size}) => (
+                                <Icon name="rocket" color={color} size={size}/>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Deplosy"
+                        component={DeployScreen}
+                        options={{
+                            title: 'Wdrssożenie',
+                            tabBarIcon: ({color, size}) => (
+                                <Icon name="rocket" color={color} size={size}/>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Deplo2y"
+                        component={DeployScreen}
+                        options={{
+                            title: 'Wdrożeni2e',
+                            tabBarIcon: ({color, size}) => (
+                                <Icon name="rocket" color={color} size={size}/>
+                            ),
+                        }}
+                    />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </GluestackUIProvider>
+    );
 };
 
-const Container = () => {
-  return (
-    <Box flex={1} backgroundColor="$black">
-      <ScrollView
-        style={{ height: '100%' }}
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        <Box
-          position="absolute"
-          sx={{
-            '@base': {
-              h: 500,
-              w: 500,
-            },
-            '@lg': {
-              h: 700,
-              w: 700,
-            },
-          }}
-        >
-          <Gradient />
-        </Box>
-        <Box
-          height="60%"
-          sx={{
-            '@base': {
-              my: '$16',
-              mx: '$5',
-              height: '80%',
-            },
-            '@lg': {
-              my: '$24',
-              mx: '$32',
-            },
-          }}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Box
-            bg="#64748B33"
-            py="$2"
-            px="$6"
-            rounded="$full"
-            alignItems="center"
-            marginTop={20}
-            sx={{
-              '@base': {
-                flexDirection: 'column',
-              },
-              '@sm': {
-                flexDirection: 'row',
-              },
-              '@md': { alignSelf: 'flex-start' },
-            }}
-          >
-            <Text color="$white" fontWeight="$normal">
-              Get started by editing
-            </Text>
-            <Text color="$white" fontWeight="$medium" ml="$2">
-              Ap333p.tsx
-            </Text>
-          </Box>
-          <Box justifyContent="center" alignItems="center">
-            <Logo />
-          </Box>
-          <Box
-            sx={{
-              '@base': {
-                flexDirection: 'column',
-              },
-              '@md': {
-                flexDirection: 'row',
-              },
-            }}
-          >
-            <FeatureCard
-              iconSvg={DocumentData}
-              name="Docs"
-              desc="Find in-depth information about gluestack features and API."
-            />
-            <FeatureCard
-              iconSvg={LightBulbPerson}
-              name="Learn"
-              desc="Learn about gluestack in an interactive course with quizzes!"
-            />
-            <FeatureCard
-              iconSvg={Rocket}
-              name="Deploy"
-              desc="Instantly drop your gluestack site to a shareable URL with vercel."
-            />
-          </Box>
-        </Box>
-      </ScrollView>
-    </Box>
-  );
-};
+export default App;
+
