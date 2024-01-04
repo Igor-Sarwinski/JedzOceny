9211c3b9ab5057a7d2ae431416a58dd42a9c960d
Igor Sarwiński <igor.s@onet.pl>
2024-01-04 14:32:18 +0100

    dodanie sensoru telefonu es ?

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -3,6 +3,8 @@ import {Box, Text, Pressable, Image} from '@gluestack-ui/themed';
 import { View } from 'react-native';
 import {config} from "../../../config/gluestack-ui.config";
 import { styles } from './styles';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 const { colors } = config.tokens;
 export const Profile = ({ navigation }: any) => {
 
@@ -16,8 +18,54 @@ export const Profile = ({ navigation }: any) => {
     const handleEditProfile = (editedItem: any) => {
         setUser({ ...user, ...editedItem });
     };
-    const handleAddPhoto = () => {
+    const handleAddPhoto = async () => {
+        try {
+            const { status } = await ImagePicker.requestCameraPermissionsAsync();
+
+            if (status !== 'granted') {
+                console.error('Camera permission not granted');
+                return;
+            }
+
+            const result = await ImagePicker.launchCameraAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 1,
+            });
+
+            console.log('ImagePicker result:', JSON.stringify(result));
+            console.log('Result cancelled:', result?.cancelled);
+            console.log('Result URI:', result?.uri);
+
+            console.log('ImagePicker result:', JSON.stringify(result));
+
+            if (!result.cancelled && result.assets && result.assets.length > 0 && result.assets[0].uri) {
+                const fileName = 'user_avatar.jpg';
+                const destination = `${FileSystem.documentDirectory}${fileName}`;
+
+                console.log('Trying to move file from', result.assets[0].uri, 'to', destination);
+
+                await FileSystem.moveAsync({
+                    from: result.assets[0].uri,
+                    to: destination,
+                });
+
+                const source = { uri: destination };
+                setUser({ ...user, avatar: source });
+
+                console.log('File moved successfully');
+            } else {
+                console.log('Image capture cancelled or no URI provided.');
+            }
+
+        } catch (error) {
+            console.error('Error taking or saving a picture', error);
+        }
     };
+
+
+
 
     return (
         <Box  style={{flex:1 ,justifyContent:"center", alignItems:"center", marginVertical:15, borderRadius:55,
